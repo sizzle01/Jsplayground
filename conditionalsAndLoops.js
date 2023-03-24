d6cd5c3f4652090b6f91aad6096f74a3f56b905f
sizzle01 <talktoaliyu@gmail.com>
2023-03-24 01:13:18 +0100

    initial commit

---
+++ conditionalsAndLoops.js
@@ -0,0 +1,12 @@
+for (i = 1; i < 11; i++){
+    if( i == 1){
+        console.log("Gold medal")
+    } else if (i==2){
+        console.log("Silver medal")
+    }else  if(i == 3){
+        console.log("Bronze medal")
+    } else if (i > 3){
+        console.log(i)
+    }
+    
+}
