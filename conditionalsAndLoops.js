c1a38d0b681a1d960b0887b9640d156a0caafdf9
sizzle01 <talktoaliyu@gmail.com>
2023-03-24 01:45:18 +0100

    I also did the exercise using switch Case

--- conditionalsAndLoops.js
+++ conditionalsAndLoops.js
@@ -1,12 +1,29 @@
-for (i = 1; i < 11; i++){
-    if( i == 1){
-        console.log("Gold medal")
-    } else if (i==2){
-        console.log("Silver medal")
-    }else  if(i == 3){
-        console.log("Bronze medal")
-    } else if (i > 3){
-        console.log(i)
-    }
+//Using for Loop
+// for (i = 1; i < 11; i++){
+//     if( i == 1){
+//         console.log("Gold medal")
+//     } else if (i==2){
+//         console.log("Silver medal")
+//     }else  if(i == 3){
+//         console.log("Bronze medal")
+//     } else if (i > 3){
+//         console.log(i)
+//     }
     
+// }
+
+//Using Switch Case;
+
+for (i = 1; i < 11; i++){
+    switch(i){
+        case 1:  console.log("Gold medal");
+        break;
+        case 2:  console.log("Silver medal");
+        break;
+        case 3:  console.log("Bronze medal");
+        break;
+        default: console.log(i);
+        }
 }
+
+
