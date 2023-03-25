bfe15ba88c4d8f60ed45677cd0a21f7bc83f41ee
sizzle01 <talktoaliyu@gmail.com>
2023-03-25 23:30:35 +0100

    I created a choose letter file for the function

--- conditionalsAndLoops.js
+++ conditionalsAndLoops.js
@@ -1,16 +1,16 @@
 //Using for Loop
-// for (i = 1; i < 11; i++){
-//     if( i == 1){
-//         console.log("Gold medal")
-//     } else if (i==2){
-//         console.log("Silver medal")
-//     }else  if(i == 3){
-//         console.log("Bronze medal")
-//     } else if (i > 3){
-//         console.log(i)
-//     }
+for (i = 1; i < 11; i++){
+    if( i == 1){
+        console.log("Gold medal")
+    } else if (i==2){
+        console.log("Silver medal")
+    }else  if(i == 3){
+        console.log("Bronze medal")
+    } else if (i > 3){
+        console.log(i)
+    }
     
-// }
+}
 
 //Using Switch Case;
 
---
+++ function.js
@@ -0,0 +1,6 @@
+// Letter finder function
+let word = "james"
+let match = 'a'
+function letterFinder(word, match){
+
+}
