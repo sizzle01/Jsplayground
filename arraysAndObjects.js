6eb5ad5a0e3e30c0e601e00a588ba95bb283a875
sizzle01 <talktoaliyu@gmail.com>
2023-03-26 02:08:25 +0100

    I practiced Arrays and objects

---
+++ arraysAndObjects.js
@@ -0,0 +1,15 @@
+var clothes = [];
+clothes.push("Tie");
+clothes.push("Suit");
+clothes.push("Pants");
+clothes.push("Jacket");
+clothes.push("Socks");
+
+console.log(clothes)
+clothes.pop();
+console.log(clothes)
+
+var favCar = {};
+favCar.color = 'red';
+favCar.convertible = false
+console.log(favCar)
